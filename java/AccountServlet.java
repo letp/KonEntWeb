944b3b2af358df3253c87485fe9cc78759bbf7a9
letp <pel1217@thi.de>
2017-05-29 21:17:39 +0200

    Update AccountServlet.java

--- java/AccountServlet.java
+++ java/AccountServlet.java
@@ -1,17 +1,26 @@
+
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class Account
  */
-@WebServlet("/Account")
+@WebServlet("/AccountServlet")
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+    @Resource(lookup="jdbc/MyTHIPool")
+    DataSource ds;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,19 +34,48 @@ public class AccountServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// Alle Angaben zu neuem Account holen
+	    request.setCharacterEncoding("UTF-8");
 	    
-	    Account neu = new Account();
+	    Account form = new Account();
 	    
-		neu.setName(request.getParameter("name"));
-		neu.setVorname(request.getParameter("vorname"));
-		neu.setEmail(request.getParameter("email"));
-		neu.setStrasse(request.getParameter("strasse"));
-		neu.setHnr(request.getParameter("hnr"));
-		neu.setStadt(request.getParameter("stadt"));
-		neu.setPlz(request.getParameter("plz"));
-		neu.setPasswort(request.getParameter("passwort"));
-		neu.setPasswort2(request.getParameter("passwort2"));
+		form.setName(request.getParameter("name"));
+		form.setVorname(request.getParameter("vorname"));
+		/**form.setEmail(request.getParameter("email"));
+		form.setStrasse(request.getParameter("strasse"));
+		form.setHnr(request.getParameter("hnr"));
+		form.setStadt(request.getParameter("stadt"));
+		form.setPlz(request.getParameter("plz"));
+		form.setPasswort(request.getParameter("passwort"));
+		form.setPasswort2(request.getParameter("passwort2"));*/
 		
+		persist(form);
+		
+		request.setAttribute("form", form);
+		
+		final RequestDispatcher dispatcher = request.getRequestDispatcher("outputaccount.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+	
+	private void persist(Account form) throws ServletException {
+	    String[] generatedkeys = new String[] {"id"};
+
+        try(Connection con = ds.getConnection();
+        PreparedStatement p = con.prepareStatement("INSERT INTO accounts (name,vorname) VALUES (?,?)",generatedkeys)){
+        
+        p.setString(1, form.getName());
+        p.setString(2, form.getVorname());
+        p.executeUpdate();
+        
+        ResultSet rs = p.getGeneratedKeys();
+        while(rs.next()){
+            form.setId(rs.getLong(1));
+        }
+        
+        } catch (Exception ex) {
+            // TODO Auto-generated catch block
+            throw new ServletException(ex.getMessage());
+        }
 	}
 
 	/**
