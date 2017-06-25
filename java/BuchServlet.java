6a591808315d3760a06f2395f0a2d67f44198dd2
letp <pel1217@thi.de>
2017-06-25 22:47:46 +0200

    Update BuchServlet.java

--- java/BuchServlet.java
+++ java/BuchServlet.java
@@ -1,9 +1,20 @@
+
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class BuchServlet
@@ -11,7 +22,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/BuchServlet")
 public class BuchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	@Resource(lookup="jdbc/MyTHIPool")
+    DataSource ds;  
+	
+    Integer i = -1;
+    ArrayList<Buch> ergebnisse = new ArrayList<Buch>();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,14 +40,57 @@ public class BuchServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	    Buch neu = new Buch();
-        
-        neu.setName(request.getParameter("name"));
-        neu.setIsbn(request.getParameter("isbn"));
-        neu.setZustand(request.getParameter("zustand"));
-        neu.setBeschreibung(request.getParameter("beschreibung"));
+	       request.setCharacterEncoding("UTF-8");
+	        HttpSession session = request.getSession();
+	        Buch buch = new Buch();
+	        
+	        buch.setTitel(request.getParameter("titel"));
+	        buch.setIsbn(request.getParameter("isbn"));
+	        buch.setZustand(request.getParameter("zustand"));
+	        buch.setBeschreibung(request.getParameter("beschreibung"));
+	        buch.setPreis(request.getParameter("preis"));
+	        
+	        ResultSet rs = null;
+	        
+	         /**
+	         * Überprüfen ob das <buch schon vorhanden ist, falls ja persistExemplar(), falls nicht persistBuch() ausführen 
+	         */
+	        try(Connection con = ds.getConnection();
+	                
+	                PreparedStatement p = con.prepareStatement("SELECT * FROM test")){
+	                
+	                
+	                rs = p.executeQuery();
+	                if(rs.next()){
+	                    request.setAttribute("buchwahl", buch);
+	                    session.setAttribute("buchwahl", buch);
+	                    exempl("test");
+	                    session.setAttribute("ergebnisse", ergebnisse);
+	                    session.setAttribute("i", i);
+	                    final RequestDispatcher dispatcher = request.getRequestDispatcher("home/html/Buch.jsp");
+	                    dispatcher.forward(request, response);
+	                }
+	        } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 	}
-
+    public void exempl(String tabelle) throws ServletException{
+        ResultSet rs = null;
+        try(Connection con = ds.getConnection();
+                PreparedStatement p = con.prepareStatement("SELECT * FROM "+tabelle)){
+                rs = p.executeQuery();
+                while(rs.next()){
+                        Buch buch = new Buch();
+                        buch.setTitel(rs.getString("titel"));
+                        ergebnisse.add(buch);
+                        i=i+1;
+                }
+         } catch (Exception ex) {
+              // TODO Auto-generated catch block
+              throw new ServletException(ex.getMessage());
+         }
+    }
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
